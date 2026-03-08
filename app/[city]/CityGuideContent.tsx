76560c6fca1a6f9d9a82ecf9257b539a1d898060
sienaindigoandlavender <siena@indigoandlavender.love>
2026-03-08 13:48:03 +0000

    Add files via upload

--- app/[city]/CityGuideContent.tsx
+++ app/[city]/CityGuideContent.tsx
@@ -302,7 +302,7 @@ export default function CityGuideContent({
         <div className="absolute inset-0 bg-gradient-to-b from-black/20 via-transparent to-black/60" />
 
         <div className="absolute bottom-0 left-0 right-0 px-8 md:px-16 lg:px-20 pb-14">
-          <h1 className="font-serif text-5xl md:text-7xl lg:text-8xl text-white">
+          <h1 className="font-serif text-3xl md:text-5xl lg:text-6xl text-white leading-[1.1]">
             {destination.title}
           </h1>
         </div>
